9520ae01daf971c0b928ebecf6a91ad6a8298488
Rameen Aamir <raamir.bscs21seecs@seecs.edu.pk>
2024-08-29 23:01:32 +0500

    style: improve CSS of Table Section

--- frontend/src/components/TabContent/TabContent.jsx
+++ frontend/src/components/TabContent/TabContent.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import TableComponent from '../Table/TableComponent.jsx';
 
 import '../Header/PricingHeader.css';
+
 function TabContent({ activeTab }) {
   return (
-    <div className="tab-content ">
-      {activeTab === 'publications' && <TableComponent />}
-      {activeTab === 'television' && <TableComponent />}
-      {activeTab === 'listicles' && <TableComponent />}
-      {activeTab === 'bestSellers' && <TableComponent />}
-      {activeTab === 'prBundles' && <TableComponent />}
-      {activeTab === 'print' && <TableComponent />}
-      {activeTab === 'socialPost' && <TableComponent />}
+    <div className="tab-content">
+      {activeTab === 'publications' && <TableComponent tableType="publications" />}
+      {activeTab === 'television' && <TableComponent tableType="television" />}
+      {activeTab === 'listicles' && <TableComponent tableType="listicles" />}
+      {activeTab === 'bestSellers' && <TableComponent tableType="bestSellers" />}
+      {activeTab === 'prBundles' && <TableComponent tableType="prBundles" />}
+      {activeTab === 'print' && <TableComponent tableType="print" />}
+      {activeTab === 'socialPost' && <TableComponent tableType="socialPost" />}
     </div>
   );
 }
