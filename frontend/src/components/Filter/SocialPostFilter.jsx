758ef3418a110c78e132215b6bc56efb362e03bc
Rameen Aamir <119052728+rameenaamir137@users.noreply.github.com>
2024-09-06 13:18:50 +0500

    Update SocialPostFilter.jsx

--- frontend/src/components/Filter/SocialPostFilter.jsx
+++ frontend/src/components/Filter/SocialPostFilter.jsx
@@ -1,1 +1,48 @@
 
+import React, { useState } from 'react';
+import './FilterSection.css';
+
+function SocialPostFilter() {
+    const genres = [
+        'Entertainment', 'Business', 'Tech', 'News', 'Luxury', 'Lifestyle', 
+        'Music', 'Fashion', 'Web 3', 'Sports', 'Real Estate', 'Political', 
+        'Gaming', 'Alcohol', 'Legal'
+      ];
+      const [selectedGenres, setSelectedGenres] = useState([]);
+      const toggleGenreSelection = (genre) => {
+        setSelectedGenres(prev =>
+          prev.includes(genre) ? prev.filter(g => g !== genre) : [...prev, genre]
+        );
+      };
+    
+    return (
+        <>
+            <div className="filter-section">
+      <div className="filter-group">
+        <label className="filter-label">Publication name</label>
+        <input type="text" placeholder="Search publication name" className="search-input" />
+      </div>
+      <div className="filter-group">
+        <label className="filter-label">Select genres</label>
+        <div className="genres">
+          {genres.map((genre) => (
+            <button
+              key={genre}
+              className={`genre-button ${selectedGenres.includes(genre) ? 'selected' : ''}`}
+              onClick={() => toggleGenreSelection(genre)}
+            >
+              {genre}
+            </button>
+          ))}
+        </div>
+      </div>
+      </div>
+        </>
+    );
+}
+
+export default SocialPostFilter;
+
+
+
+
